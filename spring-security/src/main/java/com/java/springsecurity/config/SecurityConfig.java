7a40c15d07ec51255b889a166111a7f2c4e6c3b3
SujithaChandragupthan <sujigupta16@gmail.com>
2023-10-12 14:33:40 -0700

    Spring Security JWTokens

--- spring-security/src/main/java/com/java/springsecurity/config/SecurityConfig.java
+++ spring-security/src/main/java/com/java/springsecurity/config/SecurityConfig.java
@@ -1,23 +1,32 @@
 package com.java.springsecurity.config;
 
 
+import com.java.springsecurity.filter.JWTAuthFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private JWTAuthFilter jwtAuthFilter;
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -29,10 +38,16 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/users/welcome","/users/addNewUser").permitAll()
+                .requestMatchers("/users/welcome", "/users/addNewUser","/users/authenticate").permitAll()
                 .and()
                 .authorizeHttpRequests().requestMatchers("/users/**")
-                .authenticated().and().formLogin().and().build();
+                .authenticated().and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
     @Bean
@@ -46,5 +61,10 @@ public class SecurityConfig {
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         authenticationProvider.setUserDetailsService(userDetailsService());
         return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
--- spring-security/src/main/java/com/java/springsecurity/controller/UserController.java
+++ spring-security/src/main/java/com/java/springsecurity/controller/UserController.java
@@ -1,10 +1,16 @@
 package com.java.springsecurity.controller;
 
+import com.java.springsecurity.dto.AuthRequest;
 import com.java.springsecurity.dto.Product;
 import com.java.springsecurity.entity.User;
+import com.java.springsecurity.service.JWTService;
 import com.java.springsecurity.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,26 +22,42 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JWTService jwtService;
+
     @GetMapping("/welcome")
-    public String welcomeMsg(){
+    public String welcomeMsg() {
 
         return "Welcome to User controller";
     }
 
     @PostMapping("/addNewUser")
-    public String addNewUser(@RequestBody User user){
+    public String addNewUser(@RequestBody User user) {
         return userService.addUser(user);
     }
 
     @GetMapping("/products/all")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-    public List<Product> getAllTheProduct(){
+    public List<Product> getAllTheProduct() {
         return userService.getProductList();
     }
 
     @GetMapping("/products/{id}")
     @PreAuthorize("hasAuthority('ROLE_USER')")
-    public Product getAllTheProduct(@PathVariable int id){
+    public Product getAllTheProduct(@PathVariable int id) {
         return userService.getProduct(id);
+    }
+
+    @PostMapping("/authenticate")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getUsername());
+        } else {
+            throw new UsernameNotFoundException("User name not found");
+        }
     }
 }
